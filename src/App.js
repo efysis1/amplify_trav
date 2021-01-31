741bde479ccd64d5cae6b8c0e1747fe6fe7e0a36
efysis1 <a.klementis@gmail.com>
2021-01-31 16:20:04 +0100

    sracka1
    ss

--- src/App.js
+++ src/App.js
@@ -23,4 +23,14 @@ function App() {
 }
 //4f061c1a44169ae18f010e30d0c8baf1596ea12b 
 //nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8
+/*
+Your public key has been saved in kluc_tu.pub.
+The key fingerprint is:
+SHA256:m8cr7qIH4Sxj7krs8jn6I6Bs6/+GMMvHJtr29+cmnQ8 a.klementis@gmail.com
+
+Your identification has been saved in /c/Users/aklem/.ssh/id_ed25519.
+Your public key has been saved in /c/Users/aklem/.ssh/id_ed25519.pub.
+The key fingerprint is:
+SHA256:ulB/10DikVaX9k57WgfLY8YGjD6010WNmadu7SsA39Q a.klementis@gmail.com
+*/
 export default App;
